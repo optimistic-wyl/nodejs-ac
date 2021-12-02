f53ce2fcdffb0282db25177a0afec3bf747416f8
wangyuanli <461105121@qq.com>
2021-12-02 13:47:16 +0800

    移动端h5适配

--- homework/page-h5/m1/vp-adapt.js
+++ homework/page-h5/m1/vp-adapt.js
@@ -20,20 +20,27 @@ var dpr, rem, scale;
  * 使用Flexible实现手淘H5页面的终端适配：https://github.com/amfe/article/issues/17
  * 
  * viewport：
- *  ideal viewport宽度：个人认为就是设备独立像素
+ *  layout viewport（布局视口）: docEl.clientWidth
+ *  visual viewport（视觉视口）: window.innerWidth
+ *  ideal viewport（理想视口）：screen.width,个人认为就是设备独立像素
  * 
+ * 算法：
+ * rootfontsize = screenwidth / x
+ * rem = px/设计宽度 * x
  */
-var docEl = document.documentElement;
-var fontEl = document.createElement('style');
-var metaEl = document.querySelector('meta[name="viewport"]');
 
+function setting() {
+var docEl = document.documentElement;
+var fontEl = document.getElementById('htmlFontsize') || document.createElement('style')
+fontEl.setAttribute('id', 'htmlFontsize');
+var metaEl = document.querySelector('meta[name="viewport"]');
 dpr = Math.floor(window.devicePixelRatio) || 1;
-rem = docEl.clientWidth * dpr / 10;
+rem = screen.width * dpr / 10;
 scale = 1 / dpr;
 
 // 设置viewport，进行缩放，达到高清效果
 //alert(docEl.clientWidth)
-metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+metaEl.setAttribute('content', 'initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
 //alert(docEl.clientWidth)
 // 设置data-dpr属性，留作的css hack之用
 docEl.setAttribute('data-dpr', dpr);
@@ -54,3 +61,8 @@ window.px2rem = function(v) {
 
 window.dpr = dpr;
 window.rem = rem;
+}
+setting()
+window.onresize = function(){
+    setting()
+} 
